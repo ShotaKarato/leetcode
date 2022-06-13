6494e7742837388720a2d58f5332b7260170da7d
ShotaKarato <theseuta@gmail.com>
2022-06-13 23:29:25 +0900

    removed comments from index.js

--- 13_Two_Sum/index.js
+++ 13_Two_Sum/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 //回答1
 // Forループをネスト化して組み合わせを一つずつ潰していく
-=======
-// 回答1
-// Forループをネスト化させて対応
->>>>>>> 76dda58247f264e397e2eed7accfa5ba28dae27a
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let p = 1; p < nums.length; p++) {
@@ -15,13 +10,8 @@ var twoSum = function (nums, target) {
     }
   }
 };
-<<<<<<< HEAD
-//回答2
-// 各要素ごとに処理の結果をHash Mapに記載していく
-=======
 // 回答2
 // Hash Mapで各要素の検証を行うごとにインデックスを記録
->>>>>>> 76dda58247f264e397e2eed7accfa5ba28dae27a
 var twoSum = function (nums, target) {
   const table = {};
   for (let i = 0; i < nums.length; i++) {

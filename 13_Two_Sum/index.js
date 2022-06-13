a0bd2ad6e3ef018f50e84be533f3837b1bc50aae
ShotaKarato <theseuta@gmail.com>
2022-06-13 23:17:05 +0900

    added a solution file named index.js

---
+++ 13_Two_Sum/index.js
@@ -0,0 +1,26 @@
+// 回答1
+// Forループをネスト化させて対応
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let p = 1; p < nums.length; p++) {
+      const sum = nums[i] + nums[p];
+      if (target === sum) {
+        return [i, p];
+      }
+    }
+  }
+};
+// 回答2
+// Hash Mapで各要素の検証を行うごとにインデックスを記録
+var twoSum = function (nums, target) {
+  const table = {};
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+    const diff = target - curr;
+    if (table[diff] != null) {
+      return [i, table[diff]];
+    } else {
+      table[curr] = i;
+    }
+  }
+};

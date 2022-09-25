a4a8cf2c25314d5b9384a518138414fe606ebe79
Shota Karato <theseuta@gmail.com>
2022-09-25 17:15:40 +0900

    :recycle: add another solution for 104

--- 104_Maximum_Depth_of_Binary_Tree/index.ts
+++ 104_Maximum_Depth_of_Binary_Tree/index.ts
@@ -9,15 +9,28 @@ class TreeNode {
   }
 }
 
-// Answer 1
+// Answer 1: initial answer
 type Params = TreeNode | null;
 function maxDepth(root: TreeNode | null): number {
   if (root === null) return 0;
   const dig = (node: Params, depth = 1) => {
-    const left = node && node.left !== null ? dig(node.left, depth + 1) : depth;
-    const right =
-      node && node.right !== null ? dig(node.right, depth + 1) : depth;
+    if (node === null) return depth;
+    const left = node.left !== null ? dig(node.left, depth + 1) : depth;
+    const right = node.right !== null ? dig(node.right, depth + 1) : depth;
     return left > right ? left : right;
   };
   return dig(root);
+}
+
+// Answer 2
+function maxDepth2(root: Params): number {
+  if (root === null) return 0;
+  const compare = (node: Params, counter = 1) => {
+    if (node === null) return counter;
+    return Math.max(
+      compare(node.left, counter + 1),
+      compare(node.right, counter + 1)
+    );
+  };
+  return Math.max(compare(root.left), compare(root.right));
 }

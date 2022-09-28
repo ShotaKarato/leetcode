345aa234c093c8d7cb97d86329b5a40030f22859
Shota Karato <theseuta@gmail.com>
2022-09-28 21:40:03 +0900

    :tada: add solution for add binary

---
+++ 67_Add_Binary/README.md
@@ -0,0 +1,25 @@
+# 67_Add_Binary
+
+## 💻 Description
+
+Given two binary strings a and b, return their sum as a binary string.
+
+**Example 1**
+
+```
+Input: a = "11", b = "1"
+Output: "100"
+```
+
+**Example 2**
+
+```
+Input: a = "1010", b = "1011"
+Output: "10101"
+```
+
+## 🔗 References
+
+[67 Add Binary](https://leetcode.com/problems/add-binary/)\
+[67 Add Binary explained by NeetCode](https://www.youtube.com/watch?v=keuWJ47xG8g)\
+⚠️ Above video explained this question with Python
---
+++ 67_Add_Binary/index.ts
@@ -0,0 +1,33 @@
+// Initial Answer
+const getLastChar = (str: string, index) => {
+  const previousIndex = str.length - index + 1;
+  return Number(str.substring(previousIndex, str.length - index));
+};
+
+function addBinary(a: string, b: string): string {
+  let output = "";
+  let index = 1;
+  let carryover = 0;
+
+  while (index <= a.length || index <= b.length) {
+    const lastCharOfA = getLastChar(a, index) || 0;
+    const lastCharOfB = getLastChar(b, index) || 0;
+
+    const total = carryover + lastCharOfA + lastCharOfB;
+    if (total > 1 && total % 2 === 1) {
+      output = "1" + output;
+      carryover = 1;
+    } else if (total > 1 && total % 2 === 0) {
+      output = "0" + output;
+      carryover = 1;
+    } else {
+      output = String(carryover + lastCharOfA + lastCharOfB) + output;
+      carryover = 0;
+    }
+    index++;
+  }
+  if (carryover > 0) {
+    output = "1" + output;
+  }
+  return output;
+}

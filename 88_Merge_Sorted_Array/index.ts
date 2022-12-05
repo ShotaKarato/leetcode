bf8953ecaa46b9ae5a8a723115f0a786c5cbdcd1
Shota Karato <theseuta@gmail.com>
2022-12-06 07:29:59 +0900

    :tada: add solution for 88

---
+++ 88_Merge_Sorted_Array/index.ts
@@ -0,0 +1,22 @@
+const merge = (
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void => {
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
+    if (nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1--;
+    } else {
+      nums1[i] = nums2[p2];
+      p2--;
+    }
+  }
+};

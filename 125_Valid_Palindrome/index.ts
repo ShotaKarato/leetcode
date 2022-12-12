ea43bb2c587ea3b312a7f6d89870b62a2f03d2be
Shota Karato <theseuta@gmail.com>
2022-12-13 07:34:44 +0900

    add solution for 125

---
+++ 125_Valid_Palindrome/index.ts
@@ -0,0 +1,10 @@
+const isPalindrome = (s: string): boolean => {
+  const str = s.replace(/[\W_]/g, "").toLowerCase();
+  if (str.length === 1) return true;
+  for (let i = 0; i < str.length / 2; i++) {
+    const head = str[i];
+    const tail = str[str.length - 1 - i];
+    if (head !== tail) return false;
+  }
+  return true;
+};

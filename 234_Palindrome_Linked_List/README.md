20ba9ca1ca86d1cdcb54fa64f0b75bbdeab3aff2
Shota Karato <theseuta@gmail.com>
2023-01-04 07:28:25 +0900

    :tada: add solution for 234
    add solution for 234

---
+++ 234_Palindrome_Linked_List/README.md
@@ -0,0 +1,24 @@
+# 234_Palindrome_Linked_List
+
+## 💻 Description
+
+Given the `head` of a singly linked list, return `true` if it is a
+palindrome or `false` otherwise.
+
+**Example 1**
+
+```
+Input: head = [1,2,2,1]
+Output: true
+```
+
+**Example 2**
+
+```
+Input: head = [1,2]
+Output: false
+```
+
+## 🔗 References
+
+[234_Palindrome_Linked_List](https://leetcode.com/problems/palindrome-linked-list/description/)
---
+++ 234_Palindrome_Linked_List/index.ts
@@ -0,0 +1,19 @@
+import { ListNode } from "../utils/index";
+
+const isPalindrome = (head: ListNode | null): boolean => {
+  const nums: number[] = [];
+  const trackNodeValue = (node: ListNode | null) => {
+    if (!node) return;
+    nums.push(node.val);
+    trackNodeValue(node.next);
+  };
+  trackNodeValue(head);
+  for (let i = 0; i < nums.length; i++) {
+    const start = nums[i];
+    const end = nums[nums.length - 1 - i];
+    if (start !== end) {
+      return false;
+    }
+  }
+  return true;
+};

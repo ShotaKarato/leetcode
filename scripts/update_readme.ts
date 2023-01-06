59816253e9415187fe2e39c7a256e34f270794f1
Shota Karato <theseuta@gmail.com>
2023-01-06 11:22:17 +0900

    :recycle: update script
    update script

--- scripts/update_readme.ts
+++ scripts/update_readme.ts
@@ -2,13 +2,16 @@ import { Dirent } from "fs";
 import { readdir, writeFile } from "fs/promises";
 import path from "path";
 
-type DirNameType = {
+type DirectoryName = {
   readonly questionNumber: number;
   readonly questionTitle: string;
 };
+type DirectoryMap = Record<string, DirectoryName[] | []>;
+type LinkMap = ReturnType<typeof createLinkMap>;
 
-const createWholeText = (dirNames: DirNameType[]) => {
-  const linkTexts = createLinkTexts(dirNames);
+const createWholeText = (dirMap: DirectoryMap) => {
+  const linkMap = createLinkMap(dirMap);
+  const linkText = createLinkText(linkMap);
   const wholeText = `# 超ド文系なのに気合と根性で LeetCode を解く奴
   高校時代、確率のテストで 3/100 点を叩き出して二子玉川の河川敷で答案用紙を燃やした超絶ド文系が気合と根性でLeetCodeのアルゴリズムに挑む物語です。
 
@@ -17,37 +20,63 @@ const createWholeText = (dirNames: DirNameType[]) => {
   あたたかく見守っていただけると幸いです。
 
   ## Solutions
-  ${linkTexts}
+  ${linkText}
   `;
   return wholeText;
 };
 
-const createLinkTexts = (dirNames: DirNameType[]) => {
-  return dirNames
-    .map(
-      ({ questionNumber, questionTitle }) =>
-        `[${questionNumber} ${questionTitle}](/${questionNumber}_${questionTitle.replace(
-          /\s/g,
-          "_"
-        )}/)\n\n`
-    )
-    .join("");
+const createLinkText = (linkMap: LinkMap) => {
+  let linkText: string = "";
+  for (let key in linkMap) {
+    linkText += `### ${key}
+    ${linkMap[key]}
+    `;
+  }
+  return linkText;
+};
+
+const createLinkMap = (dirMap: DirectoryMap) => {
+  const linkMap: Record<keyof DirectoryMap, string> = {};
+  for (let key in dirMap) {
+    if (dirMap[key].length > 0) {
+      linkMap[key] = dirMap[key]
+        .map(
+          ({ questionNumber, questionTitle }) =>
+            `[${questionNumber} ${questionTitle}](/${key}/${questionNumber}_${questionTitle.replace(
+              /\s/g,
+              "_"
+            )}/)\n\n`
+        )
+        .join("");
+    }
+  }
+  return linkMap;
 };
 
 const updateReadme = async () => {
   try {
-    const dirContents = await readdir(path.join(__dirname, ".."), {
-      withFileTypes: true,
-    });
-    const dirNames = dirContents
-      .filter(
-        (dirContent) => dirContent.isDirectory() && !isException(dirContent)
+    const targetDirectories = (
+      await readdir(path.join(__dirname, ".."), {
+        withFileTypes: true,
+      })
+    ).filter((targetDir) => targetDir.isDirectory() && !isException(targetDir));
+
+    const directoryMap: DirectoryMap = {};
+
+    for (let dir of targetDirectories) {
+      const directories = (
+        await readdir(dir.name, {
+          withFileTypes: true,
+        })
       )
-      .map((dir) => formatDirNames(dir.name))
-      .sort((first, second) => first.questionNumber - second.questionNumber);
+        .map((directory) => formatDirNames(directory.name))
+        .sort((first, second) => first.questionNumber - second.questionNumber);
+      directoryMap[dir.name] = directories;
+    }
+
     await writeFile(
       path.resolve(__dirname, "README.md"),
-      createWholeText(dirNames)
+      createWholeText(directoryMap)
     );
   } catch (error) {
     console.log(error);
@@ -66,7 +95,7 @@ const isException = (dirContent: Dirent) => {
   return exceptionalDirectories.some((dir) => dir === dirContent.name);
 };
 
-const formatDirNames = (dirName: string): DirNameType => {
+const formatDirNames = (dirName: string): DirectoryName => {
   const [questionNumber, ...questionTitle] = dirName.split("_");
   return {
     questionNumber: Number(questionNumber),
@@ -75,3 +104,10 @@ const formatDirNames = (dirName: string): DirNameType => {
 };
 
 updateReadme();
+
+/**
+ * {
+ *  TypeScript: {},
+ *  Rust: {}
+ * }
+ */

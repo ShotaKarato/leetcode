ee655a42d2774bd24f0c14aa3253ce7ba0c86e35
Shota Karato <theseuta@gmail.com>
2023-01-11 13:14:11 +0900

    :alien: update API schema & update graphql.ts
    update API schema & update graphql.ts

--- graphql/generated/graphql.ts
+++ graphql/generated/graphql.ts
@@ -1,15 +1,8 @@
-/* eslint-disable */
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -19,34 +12,42 @@ export type Scalars = {
   Float: number;
 };
 
-export type Query = {
-  __typename?: "Query";
-  GetStats?: Maybe<Data>;
-};
-
-export type QueryGetStatsArgs = {
-  username: Scalars["String"];
-};
-
 export type AcSubmissionNum = {
-  __typename?: "acSubmissionNum";
-  count?: Maybe<Scalars["Int"]>;
-  difficulty?: Maybe<Scalars["String"]>;
-  submissions?: Maybe<Scalars["String"]>;
-};
-
-export type Data = {
-  __typename?: "data";
-  matchedUser?: Maybe<MatchedUser>;
+  __typename?: 'AcSubmissionNum';
+  count?: Maybe<Scalars['Int']>;
+  difficulty?: Maybe<Scalars['String']>;
+  submissions?: Maybe<Scalars['String']>;
 };
 
 export type MatchedUser = {
-  __typename?: "matchedUser";
+  __typename?: 'MatchedUser';
   submitStats?: Maybe<SubmitStatsGlobal>;
-  username?: Maybe<Scalars["String"]>;
+  username?: Maybe<Scalars['String']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  matchedUser?: Maybe<MatchedUser>;
+};
+
+
+export type QueryMatchedUserArgs = {
+  username: Scalars['String'];
 };
 
 export type SubmitStatsGlobal = {
-  __typename?: Maybe<Scalars["String"]>;
+  __typename?: 'SubmitStatsGlobal';
   acSubmissionNum?: Maybe<Array<Maybe<AcSubmissionNum>>>;
 };
+
+export type Schema = {
+  __typename?: 'schema';
+  query?: Maybe<Query>;
+};
+
+export type GetUserStatsQueryVariables = Exact<{
+  username: Scalars['String'];
+}>;
+
+
+export type GetUserStatsQuery = { __typename?: 'Query', matchedUser?: { __typename?: 'MatchedUser', username?: string | null, submitStats?: { __typename?: 'SubmitStatsGlobal', acSubmissionNum?: Array<{ __typename?: 'AcSubmissionNum', difficulty?: string | null, count?: number | null, submissions?: string | null } | null> | null } | null } | null };

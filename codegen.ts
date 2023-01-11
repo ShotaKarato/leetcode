1518957a6392317b432cf300e69ad8d9ff5120c8
Shota Karato <theseuta@gmail.com>
2023-01-11 13:13:15 +0900

    :package: add plugins & update codegen config
    add plugins & update codegen config

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,17 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
+  overwrite: true,
   schema: "./graphql/schema/schema.graphql",
+  documents: ["./graphql/documents/getUserStats.graphql"],
   generates: {
-    "./graphql/generated/": {
-      preset: "client",
-      plugins: [],
+    "graphql/generated/graphql.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"],
     },
   },
 };
+
 export default config;

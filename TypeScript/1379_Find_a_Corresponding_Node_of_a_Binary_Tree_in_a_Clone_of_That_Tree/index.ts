4fb3e22cdd395eeb988e40a0d45f0f7276e4af86
Shota Karato <theseuta@gmail.com>
2023-01-13 09:25:13 +0900

    :tada: add solution for 1379
    add solution for 1379

---
+++ TypeScript/1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree/index.ts
@@ -0,0 +1,24 @@
+import { TreeNode } from "../../utils/TreeNode";
+// Initial answer
+const dig = (
+  node: TreeNode | null,
+  target: TreeNode | null
+): TreeNode | null => {
+  if (!node) return null;
+  if (!target) return null;
+  let curr: TreeNode | null;
+  if (target.val === node.val) {
+    curr = node;
+    return curr;
+  }
+  return dig(node.left, target) || dig(node.right, target);
+};
+
+const getTargetCopy = (
+  original: TreeNode | null,
+  cloned: TreeNode | null,
+  target: TreeNode | null
+): TreeNode | null => {
+  if (!original && !cloned) return null;
+  return dig(cloned, target);
+};

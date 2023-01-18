09fc5918b987d56023c1acae89d5d7f5582e4fcb
Shota Karato <theseuta@gmail.com>
2023-01-19 08:25:51 +0900

    :recycle: refactor solution for 1 to fix type error
    refactor solution for 1 to fix type error

--- TypeScript/1_Two_Sum/index.ts
+++ TypeScript/1_Two_Sum/index.ts
@@ -1,15 +1,23 @@
 type Table = {
-    [key: number]: number;
+  [key: number]: number;
 };
-const twoSum => (nums: number[], target: number): number[] => {
-    const table: Table = {};
-    for (let i = 0; i < nums.length; i++) {
-        const curr = nums[i];
-        const diff = target - curr;
-        if (table[diff] != null) {
-            return [i, table[diff]];
-        } else {
-            table[curr] = i;
-        }
+
+const twoSum = (nums: number[], target: number): number[] => {
+  const table: Table = {};
+  const output: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+    const diff = target - curr;
+    // (!table[diff]) evaluates the case in which table[diff] is 0 to be true
+    // hence explicitly checking if value is 0
+    if (table[diff] || table[diff] === 0) {
+      output.push(i);
+      output.push(table[diff]);
+    } else {
+      table[curr] = i;
     }
+  }
+  return output;
 };
+
+export {};

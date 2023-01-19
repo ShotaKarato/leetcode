dbb1cb21f62c22e43dcc0c8c952482fb79e8040a
Shota Karato <theseuta@gmail.com>
2023-01-19 23:21:01 +0900

    :recycle: refactor 13 for more type safety
    refactor 13 for more type safety

--- TypeScript/13_Roman_To_Integer/index.ts
+++ TypeScript/13_Roman_To_Integer/index.ts
@@ -1,28 +1,33 @@
 type Symbol = "I" | "V" | "X" | "L" | "C" | "D" | "M";
-type Table<T extends Symbol> = {
-    [K in T]: number;
-}
-function romanToInt(s: string): number {
-    let output: number = 0;
-    const table: Table<Symbol> = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    };
-    for (let i = 0; i < s.length; i++) {
-        const curr = s[i];
-        const next = s[i + 1];
-        console.log(curr, next);
-        if (table[curr] < table[next]) {
-            output += table[next] - table[curr];
-            i++;
-        } else {
-            output += table[curr];
-        }
+type Table = Record<Symbol, number>;
+// type Table = {
+//   [K in Symbol]: number;
+// };
+
+const romanToInt = (s: string): number => {
+  let output: number = 0;
+  const table: Table = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i] as Symbol;
+    const next = s[i + 1] as Symbol;
+    table[curr];
+    if (table[curr] < table[next]) {
+      output += table[next] - table[curr];
+      i++;
+    } else {
+      output += table[curr];
     }
-    return output;
+  }
+  return output;
 };
+
+export {};

6d11dba2c5c66f341741b7bc0326a3733eec183e
Shota Karato <theseuta@gmail.com>
2023-02-07 21:08:44 +0900

    :recycle: refactor 733
    refactor 733

--- TypeScript/733_Flood_Fill/index.ts
+++ TypeScript/733_Flood_Fill/index.ts
@@ -9,34 +9,31 @@ export const floodFill = (
   const startingPixel = image[sr][sc];
   const visitedPixels: Position[] = [[sr, sc]];
   const queue: Position[] = [[sr, sc]];
+
+  // validations
   const isVisited = (pixel: Position) =>
     visitedPixels.find(([y, x]) => y === pixel[0] && x === pixel[1]);
   const isOutOfBounds = ([y, x]: Position) =>
     x < 0 || y < 0 || x > image[0].length - 1 || y > image.length - 1;
+  // update queue
+  const visitPixel = (pixel: Position) => {
+    !isVisited(pixel) && !isOutOfBounds(pixel) && queue.push(pixel);
+  };
 
   while (queue.length > 0) {
     const [y, x] = queue.shift() as Position;
     const pixel = image[y][x];
-    console.log(y, x);
     if (pixel === startingPixel) {
       image[y][x] = color;
       visitedPixels.push([y, x]);
       // up
-      !isVisited([y + 1, x]) &&
-        !isOutOfBounds([y + 1, x]) &&
-        queue.push([y + 1, x]);
+      visitPixel([y + 1, x]);
       // right
-      !isVisited([y, x + 1]) &&
-        !isOutOfBounds([y, x + 1]) &&
-        queue.push([y, x + 1]);
+      visitPixel([y, x + 1]);
       // down
-      !isVisited([y - 1, x]) &&
-        !isOutOfBounds([y - 1, x]) &&
-        queue.push([y - 1, x]);
+      visitPixel([y - 1, x]);
       // left
-      !isVisited([y, x - 1]) &&
-        !isOutOfBounds([y, x - 1]) &&
-        queue.push([y, x - 1]);
+      visitPixel([y, x - 1]);
     }
   }
 

d04412126bcf085beeacd4ccc6126c57337aceb0
Shota Karato <theseuta@gmail.com>
2023-02-15 22:46:00 +0900

    :tada: solution for 101
    solution for 101

---
+++ TypeScript/101_Symmetric_Tree/README.md
@@ -0,0 +1,20 @@
+# 101 Symmetric Tree
+
+## 💻 Description
+
+Given the `root` of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+**Example 1**
+
+```
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+```
+
+## 🔗 References
+
+[101 Symmetric Tree](https://leetcode.com/problems/symmetric-tree/description/)
+
+[101 Symmetric Tree explained by Terrible Whiteboard](https://www.youtube.com/watch?v=3iIpnouY-bg)
+
+[101 Symmetric Tree explained by junhaowanggg](<https://leetcode.com/problems/symmetric-tree/solutions/433170/isMirror-DFS-(Recursion-OneTwo-Stacks)-+-BFS-(Queue)-Solution-in-Java/>)
---
+++ TypeScript/101_Symmetric_Tree/index.ts
@@ -0,0 +1,39 @@
+import { TreeNode } from "~/utils/TreeNode";
+
+type Params = TreeNode | null;
+
+const isSymmetric = (root: Params): boolean => {
+  if (!root) return true;
+  const compare = (left: Params, right: Params): boolean => {
+    if (left === null && right === null) return true;
+    if (left === null || right === null) return false;
+    if (left.val !== right.val) return false;
+    return compare(left.left, right.right) && compare(left.right, right.left);
+  };
+  return compare(root.left, root.right);
+};
+
+// other answers: DFS
+// const isSymmetric = (root: TreeNode | null): boolean => {
+//   if (!root) return true;
+//   if (!root.left && !root.right) return true;
+//   if (!root.left || !root.right) return false;
+
+//   const stack: (TreeNode | null)[] = [root.left, root.right];
+
+//   while (stack.length > 0) {
+//     const node1 = stack.pop() as TreeNode;
+//     const node2 = stack.pop() as TreeNode;
+
+//     if (!node1 && !node2) continue;
+//     if (!node1 || !node2) return false;
+//     if (node1.val !== node2.val) return false;
+
+//     stack.push(node1.left);
+//     stack.push(node2.right);
+//     stack.push(node1.right);
+//     stack.push(node2.left);
+//   }
+
+//   return true;
+// };

a25c387b248f316bbe26e889e88c297e6e193f20
Shota Karato <theseuta@gmail.com>
2023-02-16 21:23:23 +0900

    :recycle: refactor and add some more solutions for 700
    refactor and add some more solutions for 700

--- TypeScript/700_Search_in_a_Binary_Search_Tree/index.ts
+++ TypeScript/700_Search_in_a_Binary_Search_Tree/index.ts
@@ -1,14 +1,38 @@
 import { TreeNode } from "~/utils/TreeNode";
 
 // Solution with DFS
-export const searchBST = (
-  root: TreeNode | null,
-  val: number
-): TreeNode | null => {
-  const findTargetNode = (node: TreeNode | null): TreeNode | null => {
-    if (!node) return null;
-    if (node.val === val) return node;
-    return findTargetNode(node.left) || findTargetNode(node.right);
-  };
-  return findTargetNode(root);
+// const searchBST = (
+//   root: TreeNode | null,
+//   val: number
+// ): TreeNode | null => {
+//   const findTargetNode = (node: TreeNode | null): TreeNode | null => {
+//     if (!node) return null;
+//     if (node.val === val) return node;
+//     return findTargetNode(node.left) || findTargetNode(node.right);
+//   };
+//   return findTargetNode(root);
+// };
+
+// Solution with BFS
+// const searchBST = (root: TreeNode | null, val: number): TreeNode | null => {
+//   if (!root) return null;
+//   const queue: TreeNode[] = [root];
+//   while (queue.length > 0) {
+//     const node = queue.shift() as TreeNode;
+//     if (node.val === val) return node;
+//     node.left && queue.push(node.left);
+//     node.right && queue.push(node.right);
+//   }
+//   return null;
+// };
+
+// Possibly much more efficient solution??
+const searchBST = (root: TreeNode | null, val: number): TreeNode | null => {
+  if (!root) return null;
+  // match found
+  if (val === root.val) return root;
+  // val smaller than root.val -> left
+  if (val < root.val) return searchBST(root.left, val);
+  // val bigger than root.val -> right
+  return searchBST(root.right, val);
 };

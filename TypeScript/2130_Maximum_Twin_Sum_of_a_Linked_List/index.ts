551c2f7e54ca134e0823e8ced702f1bac95ffd5d
Shota Karato <theseuta@gmail.com>
2023-03-15 22:45:43 +0900

    :recycle: add another solution for 2130
    add another solution for 2130

--- TypeScript/2130_Maximum_Twin_Sum_of_a_Linked_List/index.ts
+++ TypeScript/2130_Maximum_Twin_Sum_of_a_Linked_List/index.ts
@@ -1,26 +1,51 @@
 import { ListNode } from "~/utils/ListNode";
 
 // Initial Answer
-export const pairSum = (head: ListNode | null): number => {
-  const nodeValues: number[] = [];
-  let curr = head;
+// export const pairSum = (head: ListNode | null): number => {
+//   const nodeValues: number[] = [];
+//   let curr = head;
 
-  while (curr) {
-    nodeValues.push(curr.val);
-    curr = curr.next;
+//   while (curr) {
+//     nodeValues.push(curr.val);
+//     curr = curr.next;
+//   }
+
+//   let p1 = 0;
+//   let p2 = nodeValues.length - 1;
+//   let maxSum = nodeValues[p1] + nodeValues[p2];
+
+//   while (p1 < p2) {
+//     p1 += 1;
+//     p2 -= 1;
+//     const sum = nodeValues[p1] + nodeValues[p2];
+//     if (sum > maxSum) {
+//       maxSum = sum;
+//     }
+//   }
+
+//   return maxSum;
+// };
+
+// Solution with fast & slow pointer and reverses list
+export const pairSum = (head: ListNode | null): number => {
+  let fastPointer = head;
+  let slowPointer = head;
+  let prev: ListNode | null = null;
+
+  while (fastPointer && fastPointer.next) {
+    fastPointer = fastPointer.next.next;
+    const next = slowPointer.next;
+    slowPointer.next = prev;
+    prev = slowPointer;
+    slowPointer = next;
   }
 
-  let p1 = 0;
-  let p2 = nodeValues.length - 1;
-  let maxSum = nodeValues[p1] + nodeValues[p2];
+  let maxSum = 0;
 
-  while (p1 < p2) {
-    p1 += 1;
-    p2 -= 1;
-    const sum = nodeValues[p1] + nodeValues[p2];
-    if (sum > maxSum) {
-      maxSum = sum;
-    }
+  while (prev && slowPointer) {
+    maxSum = Math.max(maxSum, prev.val + slowPointer.val);
+    prev = prev.next;
+    slowPointer = slowPointer.next;
   }
 
   return maxSum;

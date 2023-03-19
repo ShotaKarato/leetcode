66c9d610b11cdfbb2cfb4eb5f40819e19a62e887
Shota Karato <theseuta@gmail.com>
2023-03-19 10:50:39 +0900

    :bug: add solution for 771
    add solution for 771

---
+++ TypeScript/771_Jewels_and_Stones/index.ts
@@ -0,0 +1,20 @@
+const numJewelsInStones = (jewels: string, stones: string): number => {
+  // create map
+  const jewelMap: {
+    [K: string]: number;
+  } = {};
+  for (let i = 0; i < jewels.length; i++) {
+    const curr = jewels[i];
+    if (!jewelMap[curr]) {
+      jewelMap[curr] = 1;
+    }
+  }
+  let sum = 0;
+  for (let i = 0; i < stones.length; i++) {
+    const curr = stones[i];
+    if (jewelMap[curr]) {
+      sum += 1;
+    }
+  }
+  return sum;
+};

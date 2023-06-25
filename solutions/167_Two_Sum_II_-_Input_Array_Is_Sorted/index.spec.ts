f6d23e3e91f1b49bc655f42fc0c9d24738b1d604
Shota Karato <theseuta@gmail.com>
2023-06-26 07:15:32 +0900

    :test_tube: Refactor twoSum & add test file
    refactor twoSum & add test file

---
+++ solutions/167_Two_Sum_II_-_Input_Array_Is_Sorted/index.spec.ts
@@ -0,0 +1,8 @@
+import { twoSum } from ".";
+
+describe("twoSum", () => {
+  it("should return the pair which makes the target number", () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 2]);
+    expect(twoSum([2, 3, 4], 6)).toEqual([1, 3]);
+  });
+});
--- solutions/167_Two_Sum_II_-_Input_Array_Is_Sorted/index.ts
+++ solutions/167_Two_Sum_II_-_Input_Array_Is_Sorted/index.ts
@@ -1,14 +1,29 @@
-const twoSum = (numbers: number[], target: number): number[] => {
-  const indexMap: { [key: number]: number } = {};
-  const output: number[] = [];
-  for (let i = 0; i < numbers.length; i++) {
-    const curr = numbers[i];
-    const diff = target - curr;
-    if (indexMap[diff] !== undefined) {
-      output.push(indexMap[diff] + 1, i + 1);
+// const twoSum = (numbers: number[], target: number): number[] => {
+//   const indexMap: { [key: number]: number } = {};
+//   const output: number[] = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     const curr = numbers[i];
+//     const diff = target - curr;
+//     if (indexMap[diff] !== undefined) {
+//       output.push(indexMap[diff] + 1, i + 1);
+//     } else {
+//       indexMap[curr] = i;
+//     }
+//   }
+//   return output;
+// };
+
+export const twoSum = (numbers: number[], target: number): number[] => {
+  let head = 0;
+  let tail = numbers.length - 1;
+  while (head < tail) {
+    const sum = numbers[head] + numbers[tail];
+    if (target === sum) return [head + 1, tail + 1];
+    if (target < sum) {
+      tail--;
     } else {
-      indexMap[curr] = i;
+      head++;
     }
   }
-  return output;
+  return [];
 };

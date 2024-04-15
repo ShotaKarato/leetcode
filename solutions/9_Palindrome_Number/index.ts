dcdc47cc18c79000b4e43d81babb6fee66b1d648
Shota Karato <theseuta@gmail.com>
2024-04-15 09:18:55 +0900

    ♻️ check without making x string

--- solutions/9_Palindrome_Number/index.ts
+++ solutions/9_Palindrome_Number/index.ts
@@ -1,12 +1,20 @@
 export const isPalindrome = (x: number): boolean => {
   if (x < 0) return false;
-  const str = String(x);
-  let left = 0;
-  let right = str.length - 1;
-  while (left <= right) {
-    if (str[left] !== str[right]) return false;
-    left++;
-    right--;
+
+  let div = 1;
+  while (x >= div * 10) {
+    div *= 10;
   }
+
+  while (x) {
+    const left = x % 10;
+    const right = Math.floor(x / div);
+
+    if (left !== right) return false;
+
+    x = Math.floor((x % div) / 10);
+    div /= 100;
+  }
+
   return true;
 };

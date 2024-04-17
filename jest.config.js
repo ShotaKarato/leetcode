0773ca0c65536d986d2c199799596c3477617b0a
Shota Karato <theseuta@gmail.com>
2024-04-17 23:01:25 +0900

    🔧 fix config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
   moduleNameMapper: {
